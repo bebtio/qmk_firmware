74aa3968ed5d1541a8b73a9e9ac84cda1b3edfc6
bebtio <bebtio@gmail.com>
2025-08-02 13:06:44 -0500

    Moved the row the keys are in

--- keyboards/kinesis/keymaps/bebtio/keymap.c
+++ keyboards/kinesis/keymaps/bebtio/keymap.c
@@ -36,16 +36,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT(
-    KC_CAPS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(KEYPAD), KC_NO,
-    KC_EQL,             KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,       KC_MINS,
-    KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_BSLS,
-    LT(PRGRAM,KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,    KC_QUOT,
-    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,    KC_RSFT,
-                        KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
+    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(KEYPAD), KC_NO,
+    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,       KC_MINS,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_BSLS,
+    KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,    KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,    KC_RSFT,
+              KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
 
-                                                      KC_LCTL, KC_LALT,                                               KC_RGUI,  KC_RCTL,
-                                                               KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,  KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+                                                      LT(PRGRAM,KC_LCTL), KC_LALT,                                               KC_RGUI,  KC_RCTL,
+                                                      KC_HOME,                                               KC_PGUP,
+                                   KC_BSPC,  KC_DEL,  KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
   ),
 
   [KEYPAD] = LAYOUT(
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [PRGRAM] = LAYOUT(
     KC_NO,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,           KC_NO,        KC_NO,  KC_NO,   KC_NO,          KC_NO,   KC_NO,    KC_NO,   KC_NO,  QK_BOOT,
     KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                        KC_NO,          KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,
-    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                        LSFT(KC_EQL),   KC_LT,   KC_GT,    KC_NO,   KC_NO,  KC_NO,
+    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                        LSFT(KC_EQL),   KC_LPRN, KC_RPRN,  KC_NO,   KC_NO,  KC_NO,
     KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                        KC_EQL,         KC_LCBR, KC_RCBR,  KC_NO,   KC_NO,  KC_NO,
-    KC_LSFT, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                        KC_MINS,        KC_LPRN, KC_RPRN,  KC_NO,   KC_NO,  KC_RSFT,
+    KC_LSFT, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                        KC_MINS,        KC_LT,   KC_GT,    KC_NO,   KC_NO,  KC_RSFT,
              KC_NO,    KC_NO,    KC_NO,    KC_NO,                                                                                                  KC_NO,   KC_NO,    KC_NO,   KC_NO,
                                                       KC_TRNS,  KC_TRNS,                                               KC_TRNS,  KC_TRNS,
                                                                 KC_TRNS,                                               KC_TRNS,
